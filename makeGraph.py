c0e7dcc4b0ed467013f5a9be0faa9fe2fee201bc
Giuseppe Napoli <62253855+Peppen@users.noreply.github.com>
2020-11-30 10:30:22 +0100

    Changed the graph, now it's a bar graph, it's more understandable

--- makeGraph.py
+++ makeGraph.py
@@ -2,6 +2,7 @@ import csv
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def calculateAvarage(category):
@@ -57,12 +58,23 @@ def makePositiveNegativeRate(category):
             positive_rate = df["Positive Rate"]
             negative_rate = df["Negative Rate"]
             neutral_rate = df["Neutral Rate"]
-            plt.ylabel('Rate')
-            plt.xlabel('Iterations')
-            plt.title("Hate Offensive and Neither Rate of " + f.title())
-            plt.plot(positive_rate)
-            plt.plot(negative_rate)
-            plt.plot(neutral_rate)
+            plt.title("Results of " + category)
+            rate = ["Positive Rate", "Negative Rate", "Neutral Rate"]
+            positives = 0
+            negatives = 0
+            neutrals = 0
+            for positive in positive_rate:
+                positives += positive
+            positives = positives/len(positive_rate)
+            for negative in negative_rate:
+                negatives += negative
+            negatives = negatives/len(negative_rate)
+            for neutral in neutral_rate:
+                neutrals += neutral
+            neutrals = neutrals / len(neutral_rate)
+            results = [positives, negatives, neutrals]
+            plt.yticks(results)
+            plt.bar(rate, results)
             plt.show()
 
 
@@ -74,10 +86,21 @@ def makeHateOffensiveGraph(category):
             hate_rate = df["Hate Rate"]
             offensive_rate = df["Offensive rate"]
             neither_rate = df["Neither rate"]
-            plt.title("Hate Offensive and Neither Rate of " + f.title())
-            plt.ylabel('Rate')
-            plt.xlabel('Iterations')
-            plt.plot(hate_rate)
-            plt.plot(offensive_rate)
-            plt.plot(neither_rate)
+            plt.title("Results of " + category)
+            rate = ["Hate Rate", "Offensive Rate", "Neither Rate"]
+            hates = 0
+            offensives = 0
+            neithers = 0
+            for hate in hate_rate:
+                hates += hate
+            hates = hates/len(hate_rate)
+            for offensive in offensive_rate:
+                offensives += offensive
+            offensives = offensives/len(offensive_rate)
+            for neither in neither_rate:
+                neithers += neither
+            neithers = neithers/len(neither_rate)
+            results = [hates, offensives, neithers]
+            plt.yticks(results)
+            plt.bar(rate, results)
             plt.show()
